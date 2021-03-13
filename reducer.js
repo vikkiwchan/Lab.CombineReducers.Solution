6c17241deeaaf8b86a2020c5cb5eda921df36963
Vikki Chan <vikkiwchan@gmail.com>
2021-03-13 12:05:57 -0600

    solution

--- reducer.js
+++ reducer.js
@@ -1,28 +1,12 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import userReducer from './userReducer';
+import roomsReducer from './roomsReducer';
 
-export const AUTHENTICATED = "AUTHENTICATED";
-export const GOT_ROOMS = "GOT_ROOMS";
-export const BOOK_ROOM = "BOOK_ROOM";
+export const AUTHENTICATED = 'AUTHENTICATED';
+export const GOT_ROOMS = 'GOT_ROOMS';
+export const BOOK_ROOM = 'BOOK_ROOM';
 
-const reducer = (state = { user: {}, rooms: [] }, action) => {
-  switch (action.type) {
-    case AUTHENTICATED:
-      return { ...state, user: action.payload };
-    case GOT_ROOMS:
-      return { ...state, rooms: action.payload };
-    case BOOK_ROOM:
-      const updatedRooms = state.rooms.map(room => {
-        if (room.id === action.roomId) {
-          return { ...room, booked: true };
-        } else {
-          return room;
-        }
-      });
-
-      return { ...state, rooms: updatedRooms, user: {...state.user, bookedRoom: action.roomId } };
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+export default combineReducers({
+  user: userReducer,
+  rooms: roomsReducer,
+});
